068894dcf732a8f2b460ef7755d027db0be63080
tycoon87 <tyson.woodruff@outlook.com>
2017-10-04 17:11:23 -0700

    adds from tupls and lists

---
+++ mathDojo.py
@@ -0,0 +1,15 @@
+class Math(object):
+    def __init__(self):
+        self.result = 0
+    def add(self, *args):
+        for i in args:
+            if type(i) == list or type(i) == tuple:
+                for j in i:
+                    self.result += j
+            else:
+                self.result += i
+        print self.result
+        return self
+    
+equation1 = Math()
+equation1.add(2, 5)
